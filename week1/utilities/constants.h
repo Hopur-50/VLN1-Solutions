707ec8f42d9a83e9d63586496762c8850fcba175
biggboss83 <biggboss83@gmail.com>
2015-12-08 22:32:10 +0000

    consants cleanup

--- week1/utilities/constants.h
+++ week1/utilities/constants.h
@@ -9,8 +9,6 @@
 
 namespace constants
 {
-    const std::string DATA_FILE_NAME = "database.txt";
-    const char FILE_DELIMETER = '\t';
     const int YEAR_DIED_DEFAULT_VALUE = 13337;
     const int YEAR_OF_CONSTRUCTION_VALUE = 80083;
 
@@ -23,14 +21,7 @@ namespace constants
                                             "ORDER BY s.yearOfBirth DESC, name",
                                             "ORDER BY s.yearOfDeath, name",
                                             "ORDER BY s.yearOfDeath DESC, name"};
-    /*
-    const std::string SORT_SCIENTIST_NAME_ASCENDING = "ORDER BY s.name";
-    const std::string SORT_SCIENTIST_NAME_DESCENDING = "ORDER BY s.name DESC";
-    const std::string SORT_SCIENTIST_YEAR_BORN_ASCENDING = "ORDER BY s.yearOfBirth, name";
-    const std::string SORT_SCIENTIST_YEAR_BORN_DESCENDING = "ORDER BY s.yearOfBirth DESC, name";
-    const std::string SORT_SCIENTIST_YEAR_DIED_ASCENDING = "ORDER BY s.yearOfDeath, name";
-    const std::string SORT_SCIENTIST_YEAR_DIED_DESCENDING = "ORDER BY s.yearOfDeath DESC, name";
-    */
+
     const std::string SELECT_ALL_COMPUTERS = "SELECT name, computerType, constructed, buildYear FROM Computers c";
     const std::string SORT_COMPUTER [6] = {"ORDER BY c.name",
                                            "ORDER BY c.name DESC",
@@ -38,14 +29,7 @@ namespace constants
                                            "ORDER BY c.buildYear DESC, name",
                                            "ORDER BY c.computerType, name",
                                            "ORDER BY c.computerType DESC, name"};
-    /*
-    const std::string SORT_COMPUTER_NAME_ASCENDING = "ORDER BY c.name";
-    const std::string SORT_COMPUTER_NAME_DESCENDING = "ORDER BY c.name DESC";
-    const std::string SORT_COMPUTER_BUILD_YEAR_ASCENDING = "ORDER BY c.buildYear, name";
-    const std::string SORT_COMPUTER_BUILD_YEAR_DESCENDING = "ORDER BY c.buildYear DESC, name";
-    const std::string SORT_COMPUTER_COMPUTER_TYPE_ASCENDING = "ORDER BY c.computerType, name";
-    const std::string SORT_COMPUTER_COMPUTER_TYPE_DESCENDING = "ORDER BY c.computerType DESC, name";
-    */
+
     const std::string SORT_RELATION_SCIENTIST_ASCENDING;
     const std::string SORT_RELATION_SCIENTIST_DESCENDING;
     const std::string SORT_RELATION_COMPUTER_ASCENDING;
